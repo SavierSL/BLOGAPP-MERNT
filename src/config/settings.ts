b0972cf4cd6efcf40ab9adf859a55366a3393c33
Xavier San Lorenzo <68961639+SavierSL@users.noreply.github.com>
2021-03-02 00:49:08 +0800

    Update settings.ts

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -1,5 +1,5 @@
 export const settings = {
   mongoURI:
-    "mongodb+srv://SavierSL:xxkaa548@cluster0.qkykc.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    "mongodb+srv://SavierSL:secret123@cluster0.qkykc.mongodb.net/<dbname>?retryWrites=true&w=majority",
   jwtSecret: "xavier",
 };
